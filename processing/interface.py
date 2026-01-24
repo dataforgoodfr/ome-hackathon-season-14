2ec5b2facfecd0b4cf2bb4f93c34f18acdd317c6
Josselin Pennors <josselin.pennors@hotmail.fr>
2026-01-24 16:48:50 +0100

    update interface

--- processing/interface.py
+++ processing/interface.py
@@ -5,3 +5,19 @@ from dataclasses import dataclass
 class ChunksWithBoundaries:
     chunks: list[str]
     index_boundaries: list[int]
+
+
+@dataclass
+class Segment:
+    content: str
+    word_count: int
+    category: str | None
+
+
+@dataclass
+class TextAnalysis:
+    input_text: str
+    initial_word_count: int
+    nb_duplicates: int
+    final_word_count: int
+    segments: list[Segment]
