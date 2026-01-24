9bfc327a914da8a8fddb2494552c9ce98512d9d8
Titouan <titouan@ithaque-renovation.fr>
2026-01-24 13:01:27 +0100

    add libs with uv

---
+++ key-words/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from skimpy import skim
+
+from datasets import load_dataset
+
+ds   = load_dataset("DataForGood/ome-hackathon-season-14")
+
