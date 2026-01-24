c232c1feef59d0853ba86b3b755a1afd351ee025
Titouan <titouan@ithaque-renovation.fr>
2026-01-24 15:04:17 +0100

    key-words

--- key-words/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from skimpy import skim
-
-from datasets import load_dataset
-
-ds   = load_dataset("DataForGood/ome-hackathon-season-14")
-
